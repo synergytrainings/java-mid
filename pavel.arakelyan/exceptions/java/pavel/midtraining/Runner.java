9129520f1451eedf01a293152588561cbce587e0
pavel.arakelyan <pavel.arakelyan@arm.synisys.com>
2015-05-25 22:47:36 +0400

    Exceptions in practice

---
+++ pavel.arakelyan/exceptions/java/pavel/midtraining/Runner.java
@@ -0,0 +1,17 @@
+package pavel.midtraining;
+
+import pavel.midtraining.model.Resource;
+import pavel.midtraining.service.ResourceService;
+
+/**
+ * Class represents the use case of exceptions
+ */
+public class Runner {
+
+    public static void main(String[] args){
+        ResourceService service = new ResourceService();
+        //On < 1 number we will have our exception case
+        Resource resource = service.getResource(-1);
+        System.out.print(resource);
+    }
+}
